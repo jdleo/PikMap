eff4b1092f9e7236e19aed6fafd5488cd9790d16
John Leonardo <jdleonardo5@icloud.com>
2016-08-29 16:56:27 -0700

    location + mapkit set up

--- PikMap/ViewController.swift
+++ PikMap/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var mapCentered = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        mapView.userTrackingMode = MKUserTrackingMode.follow
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        locationAuthStatus()
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        }
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if let loc = userLocation.location {
+            if !mapCentered {
+                centerMapOnLocation(location: loc)
+                mapCentered = true
+            }
+            
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        var annotationImg: MKAnnotationView?
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            annotationImg = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationImg?.image = UIImage(named: "add_feeling_btn.png")
+        }
+        
+        return annotationImg
+    }
+    
+    @IBAction func postPik(_ sender: AnyObject) {
+        
+    }
 
 
 }
+
+
+
+
+
+
+
 
